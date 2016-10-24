6538d4e7a6c022832f57b526fc39ec0fd3ddaad2
Kate Ferleman <kateferleman@kates-macbook.attlocal.net>
2016-10-24 17:29:09 -0500

    v4

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php include_once("Bootstrap-Portfolio/index.html"); ?>
+<?php include_once("Bootstrap-Portfolio/blog.html"); ?>
+<?php include_once("Bootstrap-Portfolio/contact.html"); ?>
--- startbootstrap-business-casual-gh-pages/index.php
+++
@@ -1,3 +0,0 @@
-<?php include_once("Bootstrap-Portfolio/startbootstrap-business-casual-gh-pages/index.html"); ?>
-<?php include_once("Bootstrap-Portfolio/startbootstrap-business-casual-gh-pages/blog.html"); ?>
-<?php include_once("Bootstrap-Portfolio/startbootstrap-business-casual-gh-pages/contact.html"); ?>
