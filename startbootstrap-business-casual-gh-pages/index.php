ac179ed5978af92c3b275304dba05cdb93ef193e
Kate Ferleman <kateferleman@kates-macbook.attlocal.net>
2016-10-24 17:22:04 -0500

    v3

---
+++ startbootstrap-business-casual-gh-pages/index.php
@@ -0,0 +1,3 @@
+<?php include_once("Bootstrap-Portfolio/startbootstrap-business-casual-gh-pages/index.html"); ?>
+<?php include_once("Bootstrap-Portfolio/startbootstrap-business-casual-gh-pages/blog.html"); ?>
+<?php include_once("Bootstrap-Portfolio/startbootstrap-business-casual-gh-pages/contact.html"); ?>
