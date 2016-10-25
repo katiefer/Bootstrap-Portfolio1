d7ae2d01560a86e4a328772c164dcb05d1a0879e
Kate Ferleman <kateferleman@kates-macbook.attlocal.net>
2016-10-24 22:02:45 -0500

    v6

--- index.php
+++ index.php
@@ -1,3 +1,1 @@
-<?php include_once("Bootstrap-Portfolio1/index.html"); ?>
-<?php include_once("Bootstrap-Portfolio1/blog.html"); ?>
-<?php include_once("Bootstrap-Portfolio1/contact.html"); ?>
+<?php include_once("index.html"); ?>
